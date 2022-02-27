a8e266706ea531fe6cb6df3eafff3cd2724cd3bc
Seungwon Song <seungson@student.42seoul.kr>
2022-02-27 19:11:02 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(name='pykospacing',
 
       install_requires=[
           'tensorflow == 2.3.0',
-          'h5py == 3.1.0',
+          'h5py == 2.11.0',
           'argparse >= 1.4.0',
       ],
 
